4e9857fa08f545ae3647aeff3100caac2e9e7404
Mohammad Dababsah <aldbasmhmd028gmail.com>
2026-09-14 22:08:40 +0300

    Add basic task creation

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>
 #include "TaskManager.h"
 
 using namespace std;
@@ -6,4 +6,9 @@ using namespace std;
 void ShowTasks()
 {
     cout << "No tasks available." << endl;
+}
+
+void AddTask()
+{
+    cout << "Task added successfully." << endl;
 }
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "TaskManager.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Task Manager ===" << endl;
+
+    ShowTasks();
+    AddTask();
+
+    return 0;
+}
